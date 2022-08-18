deb16e900b427d1ea0462c98a619f16a4ee82e6c
Tomas <tomasraffom@gmail.com>
2022-08-17 22:15:49 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'LPF'
+})
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('connected to DB');
+});
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+  res.render('login')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+})
