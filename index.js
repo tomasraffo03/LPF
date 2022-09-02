c4e362b0ce3066f2c8245981b290bd6c442d2c33
Tomas <tomasraffom@gmail.com>
2022-09-02 20:28:28 -0300

    add start command

--- index.js
+++ index.js
@@ -12,9 +12,22 @@ const buttonVolverOrigen = '<a href="/home">Volver</a>';
 // const hash = crypto.createHash('sha256').update('tomasraffo').digest('hex');
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  database: 'LPF'
+  // host: 'localhost',
+  // user: 'root',
+  // database: 'LPF'
+  
+  // host: 'blcxjxn8m2yr46v2yskz-mysql.services.clever-cloud.com',
+  // user: 'udgosbqqzaafp0ip',
+  // password: 'hBuAkkTyoxyE6F9lzeXk',
+  // database: 'blcxjxn8m2yr46v2yskz'
+  
+  database: 'lpf',
+  user: 'ydjsy5l0l2sfvccc7i1b',
+  host: 'aws-sa-east-1.connect.psdb.cloud',
+  password: 'pscale_pw_Qwfg6U7azMvjfstf1nkaSKlAVUt5QkWRv9kWN6sEJh',
+  ssl: {
+    rejectUnauthorized: false
+  }
 })
 
 connection.connect((err) => {
