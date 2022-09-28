ccd7a451444ad8e14c61292dce368e4403ad163e
tomasraffo03 <72392177+tomasraffo03@users.noreply.github.com>
2022-09-28 16:47:53 -0300

    Update mailer change port

--- modules/mailer.js
+++ modules/mailer.js
@@ -4,7 +4,7 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 587,
+    port: 465,
     secure: true, 
     auth: {
         user: 'lpfmysql@gmail.com', 
